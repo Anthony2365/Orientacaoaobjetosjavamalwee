6892257ade0cd9a72cae10e7e716b15aeb6b4c5b
Anthony2365 <anthonyvinicius124@gmail.com>
2021-05-24 16:40:55 -0300

    atividade

---
+++ src/br/com/anthony/Carro.java
@@ -0,0 +1,38 @@
+package br.com.anthony;
+
+public class Carro {
+	
+	private String Marca;
+	private String Modelo;
+	private int Ano;
+	private int qtdPortas;
+	
+	
+	public String getMarca() {
+		return Marca;
+	}
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+	public String getModelo() {
+	return Modelo;
+	}
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+	public int getAno() {
+		return Ano;
+	}
+	public void setAno(int ano) {
+		Ano = ano;
+	}
+	public int getQtdPortas() {
+		return qtdPortas;
+	}
+	public void setQtdPortas(int qtdPortas) {
+		this.qtdPortas = qtdPortas;
+	}
+	
+	
+	
+}
