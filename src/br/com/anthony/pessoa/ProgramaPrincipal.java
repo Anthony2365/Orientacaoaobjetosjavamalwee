ab114f5bdfa79039a5ca85d3d36f8ca09d04d1f2
Anthony2365 <anthonyvinicius124@gmail.com>
2021-06-07 16:33:37 -0300

    Instancia dos objetos Pessoas e PessoaController

--- src/br/com/anthony/pessoa/ProgramaPrincipal.java
+++ src/br/com/anthony/pessoa/ProgramaPrincipal.java
@@ -1,8 +1,49 @@
 package br.com.anthony.pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
+		
+		List<Pessoa> pessoas = new ArrayList<>();
+		PessoaController pessoaControler = new PessoaController();
+		
+		boolean sair = false;
+		
+		
+		do {
+			
+			pessoaControler.menu();
+			
+			
+			int opcao = pessoaControler.leOpcao();
+			
+			switch(opcao) {
+			
+			case 1 : 
+				pessoas.add(pessoaControler.cadastrarPessoa());
+				break;
+				
+			case 2:
+				pessoaControler.ListarPessoas(pessoas);
+				break;
+				
+			case 9:
+				sair = true; 
+				break;
+				
+				default: 
+					System.out.println("opção invalida");
+			}
+			
+		}while(!sair); 
+		
+		
+		System.out.println("Sistema finalizado!");
+		
 		
 	}
 
