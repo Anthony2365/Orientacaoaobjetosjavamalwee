56b4bdaa6177aebebd85062dc6184025203503b1
Anthony2365 <anthonyvinicius124@gmail.com>
2021-06-10 16:37:49 -0300

    Configuração das classes para herança em pessoas

---
+++ src/br/com/anthony/pessoa/Cidade.java
@@ -0,0 +1,14 @@
+package br.com.anthony.pessoa;
+
+public class Cidade extends Estado {
+	private String nomeDaCidade;
+
+	public String getNomeDaCidade() {
+		return nomeDaCidade;
+	}
+
+	public void setNomeDaCidade(String nomeDaCidade) {
+		this.nomeDaCidade = nomeDaCidade;
+	}
+
+}
