af003b59a84ce85781f7849619a8e8b75012f0ae
Anthony2365 <anthonyvinicius124@gmail.com>
2021-06-10 16:36:43 -0300

    Refatoração  da controller para adicionar pessoa

--- src/br/com/anthony/pessoa/PessoaController.java
+++ src/br/com/anthony/pessoa/PessoaController.java
@@ -41,6 +41,39 @@ public class PessoaController {
 			System.out.println("Informe a altura: ");
 			pessoa.setAltura(tec.nextDouble());
 			
+			System.out.println("Informe o pais: ");
+			tec.nextLine();
+			pessoa.setNomeDoPais(tec.nextLine());
+			
+			System.out.println("Informe a sigla do Pais: ");
+			
+			pessoa.setSiglaDoPais(tec.nextLine());
+			
+			System.out.println("Informe o Estado: ");
+			
+			pessoa.setNomeDoEstado(tec.nextLine());
+			
+			System.out.println("Uf: ");
+			pessoa.setUf(tec.next());
+			
+			System.out.println("Informe a Cidade: ");
+			tec.nextLine();
+			pessoa.setNomeDaCidade(tec.nextLine());
+			
+			System.out.println("Informe o bairro: ");
+			
+			pessoa.setNomeDoBairro(tec.nextLine());
+			
+			System.out.println("Informe a Rua: ");
+			pessoa.setNomeDaRua(tec.nextLine());
+			
+			System.out.println("Informe o Complemento: ");
+			pessoa.setComplemento (tec.nextLine());
+			
+			System.out.println("Informe o Numero: ");
+			
+			pessoa.setNumero(tec.nextLine());
+			
 			return pessoa;
 			
 		}
@@ -48,22 +81,38 @@ public class PessoaController {
 		public List<Pessoa> ListarPessoas(List<Pessoa> pessoas) 
 		
 		{
-			System.out.printf("! %20s ! %4s ! %5s ! %6s !\n", "Nome" , "Ano" , "Idade" , "Altura"); 
+			System.out.printf("| %8s | %4s | %5s | %6s | %6s | %5s | %15s | %3s | %15s | %12s | %26s | %15s | %7s | \n", 
+					"Nome" , "Ano" , "Idade" , "Altura" , "Pais" , "sigla" , "Estado" , "Uf" , "Cidade" , "Bairro" , "Rua" , "Complemento" , "Numero" ); 
 			
 			
 			for(int i = 0; i < pessoas.size(); i++ ) {
-				System.out.printf("! %20s ! %4d ! %5d ! %6.2f !\n",
+				System.out.printf("| %8s | %4d | %5d | %6.2f | %6s | %5s | %15s | %3s | %15s | %12s | %26s | %15s | %7s | \n",
+						
 						pessoas.get(i).getNome(),
 						pessoas.get(i).getAnoDeNascimento(),
 						pessoas.get(i).getIdade(),
-						pessoas.get(i).getAltura());
-						
+						pessoas.get(i).getAltura(),
+						pessoas.get(i).getNomeDoPais(),
+				        pessoas.get(i).getSiglaDoPais(),
+						pessoas.get(i).getNomeDoEstado(),
+						pessoas.get(i).getUf(),
+						pessoas.get(i).getNomeDaCidade(),
+						pessoas.get(i).getNomeDoBairro(),
+						pessoas.get(i).getNomeDaRua(),
+						pessoas.get(i).getComplemento(),
+						pessoas.get(i).getNumero());
 				
+				
+										        
+						
 				
 		}
 			return pessoas;
 	}	
-}
+		
+					
+		}
+
 
 
 
